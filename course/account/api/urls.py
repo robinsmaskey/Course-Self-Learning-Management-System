016d2bb5c29b5d02899770fe047b3a117d36c53d
Robins Maskey <55328830+robinsmaskey@users.noreply.github.com>
2022-07-27 13:11:53 +0545

    Update urls.py

--- course/account/api/urls.py
+++ course/account/api/urls.py
@@ -6,7 +6,13 @@ app_name = 'account'
 urlpatterns = [
     path('signup/',SignupAPIView.as_view(), name = 'signup'),
     path('login/',LoginAPIView.as_view(), name = 'login'),
-    path('password/confirm/', ConfirmPasswordAPIView.as_view(), name='pwd_confirm'),
+    path('child/create/',ChildCreateAPIView.as_view(),name='create_child'),
+    # path('guardian/list/',GuardianList.as_view(), name='guardian_list'),
+    # path('child/list/',ChildList.as_view(), name='child_list'),
+    path('guardian/<int:guardian_id>/child/create/',ChildCreateAPIView.as_view(),name='create_child'),
+    # path('guardian/<int:guardian_id>/',ListAPIView.as_view(),name='child_list'),
+    # path('guardian/list/<int:guardian_id>',GuardianList.as_view(),name='guardian_list'),
+
 
 
 ]
