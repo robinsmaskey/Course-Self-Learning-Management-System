9b166640160e76d4db5b86296310a70b21218f38
Robins Maskey <55328830+robinsmaskey@users.noreply.github.com>
2022-07-27 13:08:43 +0545

    Update serializers.py

--- course/account/api/serializers.py
+++ course/account/api/serializers.py
@@ -1,3 +1,4 @@
+from tkinter import NUMERIC
 from rest_framework import serializers
 from django import forms
 from django.contrib.auth.models import User
@@ -9,22 +10,18 @@ from django.contrib.auth.password_validation import validate_password
 from django.core.validators import RegexValidator
 import re
 from django.utils.translation import gettext_lazy as _
+from account.models import Child, Guardian
 
 User = get_user_model()
 
 class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    confirm_password = serializers.CharField(write_only=True, required=True)
     
     class Meta:
         model = User
-        # password2 = CharField(label='Confirm Password')
-        fields = ('username','full_name', 'email', 'password','phone', 'user_type')
-
-    # class CustomUserDetailsSerializer(serializers.Serializer):
-
-    #     class Meta:
-    #         model = User
-    #         fields = ('confirm_password',)
-
+        fields = ('username','full_name', 'email', 'password', 'confirm_password','user_type')
+   
 
     def validate_password(self, password):
         password_validation.validate_password(password)
@@ -55,20 +52,26 @@ class SignupSerializer(serializers.ModelSerializer):
             "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"
         )
 
-
-class ConfirmPasswordSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
-
-    class Meta:
-        model = User
-        fields = ('password', 'password2')
-
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
+        if attrs['password'] != attrs['confirm_password']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
 
         return attrs
+
+
+# class ConfirmPasswordSerializer(serializers.ModelSerializer):
+#     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+#     password2 = serializers.CharField(write_only=True, required=True)
+
+#     class Meta:
+#         model = User
+#         fields = ('password', 'password2')
+
+#     def validate(self, attrs):
+#         if attrs['password'] != attrs['password2']:
+#             raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+#         return attrs
 
     
 class LoginSerializer(serializers.Serializer):
@@ -80,3 +83,33 @@ class LoginSerializer(serializers.Serializer):
         ('GUARDIAN', 'guardian'),
     )
     user_type = serializers.ChoiceField(choices=USER_TYPES,required=True)
+
+# class GuardianListSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = User
+#         fields = ['full_name', 'username', 'address', 'email', 'image', 'is_active', 'is_verified', 'user_type']
+
+
+
+class ChildListSerializer(serializers.ModelSerializer):
+    pin = serializers.IntegerField(required=True)
+    confirm_pin = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = Child
+        fields = ('full_name','username','pin', 'confirm_pin')
+
+    def validate_pin(self, pin):
+        pin = str(pin)
+        if len(pin) == 4:   
+            return pin
+        raise serializers.ValidationError('PIN must be 4 digit numbers.')
+
+    def validate(self, attrs):
+        if attrs['pin'] != attrs['confirm_pin']:
+            raise serializers.ValidationError({"pin": "PIN didn't match."})
+
+        return attrs
+
+
